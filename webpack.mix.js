100d2a666d1da7cdc0aa0941040d9a33f42fac61
Davi Bernardo <davi.bernardo@sbahq.org>
2024-07-17 22:24:36 -0300

    Configuração do laravel 9 com view.js 2.6

---
+++ webpack.mix.js
@@ -0,0 +1,9 @@
+const mix = require('laravel-mix');
+
+
+mix.js('resources/js/app.js', 'public/js')
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        postCss: [require('autoprefixer'), require('postcss-rtl')],
+    })
