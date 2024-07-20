83ffb9e368701c497de5c058ebcad68c2fce25ae
Davi Bernardo <davi.bernardo@sbahq.org>
2024-07-20 12:21:52 -0300

    Melhorias no layout e criando o formulário de cadastro das tags

--- resources/js/common.js
+++ resources/js/common.js
@@ -16,6 +16,37 @@ export default {
             } catch (error) {
                 return error
             }
-        }
+        },
+
+        i(desc, title="Hey") {
+            this.$Notice.info({
+                title: title,
+                desc: desc
+            });
+        },
+        s(desc, title="Great!") {
+            this.$Notice.success({
+                title: title,
+                desc: desc
+            });
+        },
+        w(desc, title="Oops!") {
+            this.$Notice.warning({
+                title: title,
+                desc: desc
+            });
+        },
+        e(desc, title="Oops!") {
+            this.$Notice.error({
+                title: title,
+                desc: desc
+            });
+        }, 
+        swr(desc='Algo deu errado! Por favor, tente novamente.', title="Oops") {
+            this.$Notice.error({
+                title: title,
+                desc: desc
+            });
+        }, 
     },
 }
--- resources/js/components/pages/tags.vue
+++ resources/js/components/pages/tags.vue
@@ -3,7 +3,7 @@
        <div class="content">
 			<div class="container-fluid">
 				<div class="_1adminOverveiw_table_recent _box_shadow _border_radious _mar_b30 _p20">
-					<p class="_title0">Tags <button class="_btn make_btn2" type="button">Add Tag</button></p>
+					<p class="_title0">Tags <button class="_btn make_btn2" type="button" @click="addModal=true">Add Tag</button></p>
 
 					<div class="_overflow _table_div">
 						<table class="_table">
@@ -25,19 +25,57 @@
 						</table>
 					</div>
 				</div>
+				<!-- tag adding modal -->
+				<Modal
+					v-model="addModal"
+					title="Add tag"
+					:mask-closable="false"
+					:closable="false"
+					>
+					<input v-model="data.tagName" placeholder="Adicionar tag" >
+					<div class="footer-modificado">
+						<Button type="default" @click="addModal=false">Fechar</Button>
+						<Button class="_btn make_btn2" @click="addTag" :disabled="idAdding" :loading="idAdding">{{ idAdding ? 'Adicionando' : 'Adicionar' }}</Button>
+					</div>
+				</Modal>
 			</div>
 		</div>
     </div>
 </template>
 <script>
+import { Modal } from 'bootstrap';
+
 export default {
-	async created() {
-		const res = await this.callApi('post', 'app/create_tag', {tagName: 'testtag'});
-		if(res.status==200){
-			console.log(res)
-		}else{
-			console.log(res)
+	
+	data() {
+		return {
+			data: {
+				tagName: ''
+			},
+			addModal: false,
+			idAdding:false
 		}
 	},
+
+	methods: {
+		addTag(){
+			if(this.data.tagName.trim()=='') return this.e('O nome da tag é obrigatório')
+			const res = await this.callApi('post', 'app/create_tag', this.data)
+			if(res.status===200){
+				this.s('Tag Adicionada com sucesso')
+			}else{
+				this.swr()
+			}
+			
+		}
+	},
+	// async created() {
+	// 	const res = await this.callApi('post', 'app/create_tag', {tagName: 'testtag'});
+	// 	if(res.status==200){
+	// 		console.log(res)
+	// 	}else{
+	// 		console.log(res)
+	// 	}
+	//},
 }
 </script>
